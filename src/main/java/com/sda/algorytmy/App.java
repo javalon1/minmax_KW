a64d4cc784c4e2e1a1614460578c319eed613c8b
Karolina Wanat <carolinawanat@gmail.com>
2019-05-12 13:02:30 +0100

    min max elements

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,8 +1,38 @@
 package com.sda.algorytmy;
 
-/
+
+import static sun.swing.MenuItemLayoutHelper.max;
+
 public class App {
     public static void main(String[] args) {
+        int[] arr = new int[]{4, 6, 2, 1, 3};
 
+        min(arr);
+        max(arr);
+
+    }
+
+    private static void min(int[] arr) {
+        int a = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < a) {
+                a= arr[i];
+            }
+
+
+        }
+        System.out.println("element minimalny " + a);
+    }
+
+    public static void max(int[] arr){
+        int a = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > a) {
+                a= arr[i];
+            }
+
+
+        }
+        System.out.println("element maksymalny " + a);
     }
 }
